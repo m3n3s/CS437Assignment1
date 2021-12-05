9e36863ea991c2abbfff800f2c9a25d4e039b1bc
Enes Battal <52421169+m3n3s@users.noreply.github.com>
2021-12-05 14:20:03 +0300

    Initial commit

---
+++ XSSInjection/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    data = request.POST.get('name')
+    print(data)
+
+    return render(request, 'vulnerable.html', {'data': data})
+
